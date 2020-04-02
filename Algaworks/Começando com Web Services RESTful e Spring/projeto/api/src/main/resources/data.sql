26b5c7a87698ff08873dc7bf1bc37798562d539d
Rodrigo Rogovski <rfrogovski@gmail.com>
2020-04-02 08:29:31 -0400

    Novos inserts

--- Algaworks/Começando com Web Services RESTful e Spring/projeto/api/src/main/resources/data.sql
+++ Algaworks/Começando com Web Services RESTful e Spring/projeto/api/src/main/resources/data.sql
@@ -5,3 +5,7 @@ INSERT INTO AUTOR(nome, nascimento, nacionalidade) VALUES('Autor 03', '1980-01-0
 INSERT INTO LIVRO(nome, autor_id, publicacao, editora, resumo) VALUES('Livro 1', 1, '2020-01-01 00:00:00', 'Editora 01', 'Resumo do livro 01');
 INSERT INTO LIVRO(nome, autor_id, publicacao, editora, resumo) VALUES('Livro 2', 2, '2020-01-01 00:00:00', 'Editora 02', 'Resumo do livro 02');
 INSERT INTO LIVRO(nome, autor_id, publicacao, editora, resumo) VALUES('Livro 3', 3, '2020-01-01 00:00:00', 'Editora 03', 'Resumo do livro 03');
+
+INSERT INTO COMENTARIO(data, texto, usuario, livro_id) VALUES('2020-01-01 00:00:00','Comentário do livro 01', 'usuario 01', 1);
+INSERT INTO COMENTARIO(data, texto, usuario, livro_id) VALUES('2020-01-01 00:00:00','Comentário do livro 01', 'usuario 02', 1);
+INSERT INTO COMENTARIO(data, texto, usuario, livro_id) VALUES('2020-01-01 00:00:00','Comentário do livro 02', 'usuario 01', 2);
