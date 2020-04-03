4dd144b51936134f7119fc0fbd972db7f54ffb43
Rodrigo Rogovski <rfrogovski@gmail.com>
2020-04-03 15:49:13 -0400

    Negociação de conteúdos com Media Type

--- Algaworks/Começando com Web Services RESTful e Spring/projeto/api/src/main/java/com/rrogovski/api/resources/AutoresResource.java
+++ Algaworks/Começando com Web Services RESTful e Spring/projeto/api/src/main/java/com/rrogovski/api/resources/AutoresResource.java
@@ -10,6 +10,7 @@ import com.rrogovski.api.domain.Autor;
 import com.rrogovski.api.services.AutoresService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +30,7 @@ public class AutoresResource {
   @Autowired
   private AutoresService autoresService;
 
-  @GetMapping
+  @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE} )
   public ResponseEntity<List<Autor>> listar() {
     return ResponseEntity.ok(autoresService.listar());
   }
