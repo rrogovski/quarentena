779d89cb4f786b2ac655b254ac542d14e1050e81
Rodrigo Rogovski <rfrogovski@gmail.com>
2020-04-03 18:53:43 -0400

    Configuração do app para response padrão json

--- Algaworks/Começando com Web Services RESTful e Spring/projeto/api/src/main/java/com/rrogovski/api/config/AppConfiguration.java
+++ Algaworks/Começando com Web Services RESTful e Spring/projeto/api/src/main/java/com/rrogovski/api/config/AppConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * AppConfiguration
@@ -13,13 +14,31 @@ public class AppConfiguration implements WebMvcConfigurer {
 
   @Override
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-    configurer.favorPathExtension(false).
-      favorParameter(true).
+    configurer.favorParameter(true).
       parameterName("mediaType").
-      ignoreAcceptHeader(true).
-      useJaf(false).
+      ignoreAcceptHeader(false).
       defaultContentType(MediaType.APPLICATION_JSON).
       mediaType("xml", MediaType.APPLICATION_XML).
       mediaType("json", MediaType.APPLICATION_JSON);
   }
 }
+
+// @Configuration
+// public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+// @Override
+// public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+//     //set path extension to true
+//     configurer.favorPathExtension(false).
+//     //set favor parameter to false
+//     favorParameter(false).
+//     //ignore the accept headers
+//     ignoreAcceptHeader(false).
+//     //dont use Java Activation Framework since we are manually specifying the mediatypes required below
+//     useJaf(false).
+//     defaultContentType(MediaType.APPLICATION_JSON).
+//     mediaType("xml", MediaType.APPLICATION_XML).
+//     mediaType("json", MediaType.APPLICATION_JSON);
+//   }
+// }
--- Algaworks/Começando com Web Services RESTful e Spring/projeto/api/src/main/java/com/rrogovski/api/resources/LivroResources.java
+++ Algaworks/Começando com Web Services RESTful e Spring/projeto/api/src/main/java/com/rrogovski/api/resources/LivroResources.java
@@ -14,10 +14,13 @@ import com.rrogovski.api.services.exceptions.LivroNaoEncontratoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,14 +37,14 @@ public class LivroResources {
 	@Autowired
 	private LivrosService livrosService;
 
-  @RequestMapping(method = RequestMethod.GET)
+  @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<List<Livro>> listar() {
 		CacheControl cacheControl = CacheControl.maxAge(20, TimeUnit.SECONDS);
 		// return ResponseEntity.ok(livrosService.listar());
 		return ResponseEntity.status(HttpStatus.OK).cacheControl(cacheControl).body(livrosService.listar());
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public ResponseEntity<Livro> salvar(@Valid @RequestBody Livro livro) {		
 		livro = livrosService.salvar(livro);
 
@@ -49,19 +52,19 @@ public class LivroResources {
 		return ResponseEntity.created(uri).body(livro);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Livro> buscar(@PathVariable("id") Long id) {
 		CacheControl cacheControl = CacheControl.maxAge(20, TimeUnit.SECONDS);
 		// return ResponseEntity.ok(livrosService.buscar(id));
 		return ResponseEntity.status(HttpStatus.OK).cacheControl(cacheControl).body(livrosService.buscar(id));
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Livro> deletar(@PathVariable("id") Long id) {		
 		return ResponseEntity.ok(livrosService.deletar(id));
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	@PutMapping(value = "/{id}")
 	public ResponseEntity<Livro> atualizar(@PathVariable("id") Long id, @RequestBody Livro livro) {
 		try {
 			return ResponseEntity.ok(livrosService.atualizar(livro));
